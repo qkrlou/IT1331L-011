17c7593308c1b6078bbab8c2d9ca9824193b1fec
Louise <adrianne.petras@uap.asia>
2023-10-19 18:42:59 +0800

    Initial commit

---
+++ registration-api/registration-api/src/main/java/com/uap/it1311l/registrationapi/controller/RegistrationController.java
@@ -0,0 +1,29 @@
+package com.uap.it1311l.registrationapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.uap.it1311l.registrationapi.model.Attendee;
+import com.uap.it1311l.registrationapi.repository.RegistrationMybatisRepository;
+
+@RestController
+public class RegistrationController {
+	@Autowired
+	RegistrationMybatisRepository registrationRepo;
+	
+	@PostMapping("/event/register")
+	public Attendee register(@RequestBody Attendee student) {
+		registrationRepo.insert(student);
+		return student; 
+	}
+	
+	@GetMapping("/attendees")
+    public List<Attendee> getList() {
+        return registrationRepo.findAll();
+    }
+}
---
+++ registration-api/registration-api/src/main/java/com/uap/it1311l/registrationapi/model/Attendee.java
@@ -0,0 +1,48 @@
+package com.uap.it1311l.registrationapi.model;
+
+public class Attendee {
+	public String attendeeId;
+	public String getAttendeeId() {
+		return attendeeId;
+	}
+	public void setAttendeeId(String attendeeId) {
+		this.attendeeId = attendeeId;
+	}
+	public String firstName;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String middleName;
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String lastName;
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String course;
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public int age;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
